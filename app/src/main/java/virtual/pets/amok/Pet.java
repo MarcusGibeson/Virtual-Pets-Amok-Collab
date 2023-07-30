bbe2b312aa82580da47868a813bafc3318afcb8f
Jose Carmona <manuel_andeliz@hotmail.com>
2023-07-30 16:33:11 -0400

    nkkln

---
+++ app/src/main/java/virtual/pets/amok/Pet.java
@@ -0,0 +1,69 @@
+package virtual.pets.amok;
+
+public class Pet {
+    private String name;
+    private String oil;
+    private String recharge;
+    private String feed;
+    private String thirst;
+    private String waste;
+    private String play;
+
+    public Pet(String name, String oil, String recharge, String feed, String waste, String play) {
+        this.oil = oil;
+        this.recharge = recharge;
+        this.feed = feed;
+        this.waste = waste;
+        this.play = play;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOil() {
+        return oil;
+    }
+
+    public void setOil(String oil) {
+        this.oil = oil;
+    }
+
+    public String getRecharge() {
+        return recharge;
+    }
+
+    public void setRecharge(String recharge) {
+        this.recharge = recharge;
+    }
+
+    public String getFeed() {
+        return feed;
+    }
+
+    public void setFeed(String feed) {
+        this.feed = feed;
+    }
+
+    public String getWaste() {
+        return waste;
+    }
+
+    public void setWaste(String waste) {
+        this.waste = waste;
+    }
+
+    public String getPlay() {
+        return play;
+    }
+
+    public void setPlay(String play) {
+        this.play = play;
+    }
+
+}
--- app/src/main/java/virtual/pets/amok/PetShelter.java
+++ app/src/main/java/virtual/pets/amok/PetShelter.java
@@ -1,5 +1,6 @@
 package virtual.pets.amok;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -7,8 +8,24 @@ public class PetShelter {
     /*
      * Create a hashmap of robots and live pets
      */
-    Map <Integer, petClass> shelterAnimals = new HashMap<Integer, petClass>();
+    Map <Integer, Pet> shelterAnimals = new HashMap<Integer, Pet>();
     Scanner input = new Scanner(System.in);
+    
+    /*
+     * Adds pet to shelter
+     */
+
+    public void addPet(Pet VirtualPet) throws Exception {
+        if(!shelterAnimals.containsKey(VirtualPet.getPetID())) {
+            shelterAnimals.put(VirtualPet.getPetID(), VirtualPet);
+        } else {
+            throw new Exception ("Invalid name");
+        }
+    }
+
+    //random text
+
+
     
 
 
