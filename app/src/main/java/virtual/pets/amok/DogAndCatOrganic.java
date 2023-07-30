dbfed5582277e2203942c1ec7823538445152c4b
Jose Carmona <manuel_andeliz@hotmail.com>
2023-07-30 14:56:03 -0400

    change 1

---
+++ app/src/main/java/virtual/pets/amok/DogAndCatOrganic.java
@@ -0,0 +1,5 @@
+public class DogAndCatOrganic extends Shelter {
+    DogAndCatOrganic(String name, String oil, String recharge, String feed, String waste, String play){
+        super(name, oil, recharge, feed, waste, play);
+    }
+}
---
+++ app/src/main/java/virtual/pets/amok/DogAndCatRobots.java
@@ -0,0 +1,7 @@
+public class DogAndCatRobots extends Shelter {
+    DogAndCatRobots(String name, String oil, String recharge, String feed, String waste, String play){
+        super(name, oil, recharge, feed, waste, play);
+    }
+
+}
+
