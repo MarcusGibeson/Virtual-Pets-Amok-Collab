e9930e2244fb37c37b76c0e8007bec5ef0458447
Marcus Gibeson <133388744+MarcusGibeson@users.noreply.github.com>
2023-07-30 14:46:09 -0400

    commit

---
+++ app/src/main/java/virtual/pets/amok/PetShelter.java
@@ -0,0 +1,14 @@
+package virtual.pets.amok;
+
+import java.util.Map;
+
+public class PetShelter {
+    /*
+     * Create a hashmap of robots and live pets
+     */
+    Map <Integer, petClass> shelterAnimals = new HashMap<Integer, petClass>();
+    
+    
+
+
+}
