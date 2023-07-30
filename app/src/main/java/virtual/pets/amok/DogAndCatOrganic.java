5b0d8377b685934d296e86fc923b360ac2adab17
Jose Carmona <manuel_andeliz@hotmail.com>
2023-07-30 15:25:52 -0400

    change 3

--- app/src/main/java/virtual/pets/amok/DogAndCatOrganic.java
+++ app/src/main/java/virtual/pets/amok/DogAndCatOrganic.java
@@ -1,6 +1,6 @@
 package virtual.pets.amok;
 
-public class DogAndCatOrganic extends Shelter {
+public class DogAndCatOrganic extends Pet {
     DogAndCatOrganic(String name, String oil, String recharge, String feed, String waste, String play){
         super(name, oil, recharge, feed, waste, play);
     }
---
+++ app/src/main/java/virtual/pets/amok/DogAndCatRobot.java
@@ -0,0 +1,5 @@
+public class DogAndCatRobot extends Pet {
+    DogAndCatRobot(String name, String oil, String recharge, String feed, String waste, String play){
+        super(name, oil, recharge, feed, waste, play);
+    }
+}
