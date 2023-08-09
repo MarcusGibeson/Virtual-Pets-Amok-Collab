20a216f517429e971d252cf6a43d07f8ce3e35e3
Dtimo33 <134143620+Dtimo33@users.noreply.github.com>
2023-08-09 17:03:07 -0400

    Co-authored-by: Marcus Gibeson <MarcusGibeson@users.noreply.github.com> Co-authored-by: LukretiaD <LukretiaD@users.noreply.github.com> Co-authored-by: Kerwyn Daniel <Kerwyn-Daniel@users.noreply.github.com>

--- app/src/main/java/virtual/pets/amok/PetLive.java
+++ app/src/main/java/virtual/pets/amok/PetLive.java
@@ -1,7 +1,10 @@
 package virtual.pets.amok;
 
-public class PetLive extends Pet { // this should extend Pet
+import java.util.Scanner;
 
+public class PetLive extends Pet { 
+    
+    Scanner input = new Scanner(System.in);
     public int hunger;
     public int thirst;
     public int waste;
@@ -58,5 +61,44 @@ public class PetLive extends Pet { // this should extend Pet
         hunger += 2;
         boredom += 2;
     }
+    public void playPet(){
+        thirst += 5;
+        hunger += 5;
+        boredom -= 10;
+    }
 
+    public void tick() {
+        if (this.hunger < 80) {
+            System.out.println(this.name + " is starving!");
+            System.out.println("Press enter to continue");
+            input.nextLine();
+        }
+        else if (this.thirst < 80) {
+            System.out.println(this.name + " is parched!");
+            System.out.println("Press enter to continue");
+            input.nextLine();
+        }
+        else if (this.boredom < 80) {
+            System.out.println(this.name + " is dying of boredom!");
+            System.out.println("Press enter to continue");
+            input.nextLine();
+        }
+    }
+
+    public void deathTick() {
+        if (this.hunger >= 100) {
+            System.out.println(this.name + " has died from starvation...");
+            System.out.println("Press enter to continue");
+            input.nextLine();
+        } else if (this.thirst >= 100) {
+            System.out.println(this.name + " has died from dehydration...");
+            System.out.println("Press enter to continue");
+            input.nextLine();
+        } else if (this.boredom >= 100) {
+            System.out.println(this.name + " has died from dehydration...");
+            System.out.println("Press enter to continue");
+            input.nextLine();
+        }
+        this.name = "DEAD";
+    }
 }
--- app/src/main/java/virtual/pets/amok/PetRobot.java
+++ app/src/main/java/virtual/pets/amok/PetRobot.java
@@ -47,4 +47,59 @@ public class PetRobot extends Pet {
     public void setClean(int clean) {
         this.clean = clean;
     }
+
+        public void oilRobot() {
+        oil += 10;
+        clean -= 2;
+    }
+
+    //robot pet actions
+        public void roboDance() {
+        recharge -= 20;
+        oil -= 25;
+        repair -=10;
+        clean -=5;
+        System.out.println("Domo Arigato Mister Roboto!");
+    }
+
+    public void roboLaser() {
+        recharge -= 35;
+        oil -= 15;
+        repair -=10;
+        System.out.println("Pew-Pew, do not aim for the eyes!");
+    }
+
+    //repair and clean methods
+    public void repairRobot(){
+        oil = 75;
+        repair = 75;
+        System.out.println("Would you like to apply for a warranty?");
+    }
+    public void cleanRobot(){
+        recharge = 75;
+        clean = 75;
+        System.out.println("It has the new car smell! ");
+    }
+
+    //robot tick method
+    public void robTick(){
+        recharge -= 4;
+        oil -= 2;
+        clean -= 7;
+
+        if (oil<=15){
+            System.out.println("System Alert: Low oil Detected!");
+        }
+        if(recharge<=5){
+            System.out.println("System Alert : Low Battery, Please Charge!");
+        }
+                if(repair<=5){
+            System.out.println("System Alert : Malfunction Detected!");
+        }
+                if(clean<=10){
+            System.out.println("System Alert : Sensor Block Detected!");
+        }
+
+    }
+
 }
