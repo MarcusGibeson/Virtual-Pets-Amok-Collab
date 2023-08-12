6d38c77471a5e3d50aa2b09cc51f9db1123b1445
Marcus Gibeson <133388744+MarcusGibeson@users.noreply.github.com>
2023-08-12 02:35:19 -0400

    color

--- app/src/main/java/virtual/pets/amok/Menus.java
+++ app/src/main/java/virtual/pets/amok/Menus.java
@@ -3,16 +3,22 @@ package virtual.pets.amok;
 import java.util.Scanner;
 
 public class Menus {
-    
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
     Scanner input = new Scanner(System.in);
+
     public String mainMenu(){
         
         
-        System.out.println("1. Interact with Pets");
-        System.out.println("2. Interact with Robots");
-        System.out.println("3. List all pets");
-        System.out.println("0. Exit");
-        System.out.print("\nSelect an Option? ");
+        System.out.println(ANSI_GREEN + "1. Interact with Pets" + ANSI_RESET);
+        System.out.println(ANSI_GREEN + "2. Interact with Robots" + ANSI_RESET);
+        System.out.println(ANSI_GREEN + "3. List all pets" + ANSI_RESET);
+        System.out.println(ANSI_GREEN + "0. Exit" + ANSI_RESET);
+        System.out.print(ANSI_YELLOW + "\nSelect an Option? " + ANSI_RESET);
         String option = input.nextLine();
         
         return option;
@@ -21,15 +27,15 @@ public class Menus {
     public String livePetMenu(){
         
         
-        System.out.println("1. Admit New Cat");
-        System.out.println("2. Admit New Dog");
-        System.out.println("3. Feed Pets");
-        System.out.println("4. Give Pets Water");
-        System.out.println("5. Play With Pet");
-        System.out.println("6. Adopt out Pet");
-        System.out.println("7. Clean litter box or cage");
-        System.out.println("0. Return to Main Menu?");
-        System.out.print("\nSelect an Option? ");
+        System.out.println(ANSI_CYAN + "1. Admit New Cat" + ANSI_RESET);
+        System.out.println(ANSI_CYAN + "2. Admit New Dog" + ANSI_RESET);
+        System.out.println(ANSI_CYAN + "3. Feed Pets" + ANSI_RESET);
+        System.out.println(ANSI_CYAN + "4. Give Pets Water" + ANSI_RESET);
+        System.out.println(ANSI_CYAN + "5. Play With Pet" + ANSI_RESET);
+        System.out.println(ANSI_CYAN + "6. Adopt out Pet" + ANSI_RESET);
+        System.out.println(ANSI_CYAN + "7. Clean litter box or cage" + ANSI_RESET);
+        System.out.println(ANSI_CYAN + "0. Return to Main Menu?" + ANSI_RESET);
+        System.out.print(ANSI_YELLOW + "\nSelect an Option? " + ANSI_RESET);
         
         String value = input.nextLine();
         
@@ -37,9 +43,9 @@ public class Menus {
     }
 
     public String dogOrCatMenu() {
-        System.out.println("1. Clean Cat Litter Boxes");
-        System.out.println("2. Clean Dog cages");
-        System.out.println("0. Return to Live Pet Menu?");
+        System.out.println(ANSI_PURPLE + "1. Clean Cat Litter Boxes" + ANSI_RESET);
+        System.out.println(ANSI_PURPLE + "2. Clean Dog cages" + ANSI_RESET);
+        System.out.println(ANSI_PURPLE + "0. Return to Live Pet Menu?" + ANSI_RESET);
 
         String value = input.nextLine();
         return value;
@@ -48,16 +54,16 @@ public class Menus {
     public String robotPetMenu(){
         
         
-        System.out.println("1. Create Robot Pet");
-        System.out.println("2. Recharge Battery");
-        System.out.println("3. Give Pets Oil");
-        System.out.println("4. Make Robot dance");
-        System.out.println("5. Make Robot shoot laser!");
-        System.out.println("6. Repair Robot pets");
-        System.out.println("7. Clean Robot pets");
-        System.out.println("8. Sell Robot Pet");
-        System.out.println("0. Return to Main Menu?");
-        System.out.print("\nSelect an Option? ");
+        System.out.println(ANSI_RED + "1. Create Robot Pet" + ANSI_RESET);
+        System.out.println(ANSI_RED + "2. Recharge Battery" + ANSI_RESET);
+        System.out.println(ANSI_RED + "3. Give Pets Oil" + ANSI_RESET);
+        System.out.println(ANSI_RED + "4. Make Robot dance" + ANSI_RESET);
+        System.out.println(ANSI_RED + "5. Make Robot shoot laser!" + ANSI_RESET);
+        System.out.println(ANSI_RED + "6. Repair Robot pets" + ANSI_RESET);
+        System.out.println(ANSI_RED + "7. Clean Robot pets" + ANSI_RESET);
+        System.out.println(ANSI_RED + "8. Sell Robot Pet" + ANSI_RESET);
+        System.out.println(ANSI_RED + "0. Return to Main Menu?" + ANSI_RESET);
+        System.out.print(ANSI_YELLOW + "\nSelect an Option? " + ANSI_RESET);
         
         String value = input.nextLine();
         
